e5ed0f5562a23b0ffae05f3fad41ec1df30ed8c0
Adrian Eguez <eadepto@hotmail.com>
2020-07-09 08:09:17 -0500

    Terminado ciclo de vida

--- 02-android/app/src/main/java/com/example/moviles/CicloVida.kt
+++ 02-android/app/src/main/java/com/example/moviles/CicloVida.kt
@@ -2,6 +2,7 @@ package com.example.moviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_ciclo_vida.*
 
@@ -51,6 +52,34 @@ class CicloVida : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i("Activity", "OnStop")
+        Log.i("Activity", "OnDestroy")
     }
+
+    override fun onSaveInstanceState(
+        outState: Bundle
+    ) {
+        Log.i("activity", "onSaveInstanceState")
+        outState?.run {
+            putInt("numeroActualGuardado", numeroActual)
+        }
+        super.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(
+        savedInstanceState: Bundle
+    ) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.i("activity", "onRestoreInstanceState")
+        val valorRecuperado = savedInstanceState
+            ?.getInt("numeroActualGuardado")
+        
+        if(valorRecuperado != null){
+            this.numeroActual = valorRecuperado
+            tv_numero.text = this.numeroActual.toString()
+        }
+    }
+
+
+
+
 }
