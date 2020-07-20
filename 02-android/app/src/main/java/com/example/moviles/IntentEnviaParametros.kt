20b5c07d7e1c2928ca1be18e1ca63feeccf54862
Adrian Eguez <eadepto@hotmail.com>
2020-07-20 07:40:04 -0500

    recibiendo parametros

--- 02-android/app/src/main/java/com/example/moviles/IntentEnviaParametros.kt
+++ 02-android/app/src/main/java/com/example/moviles/IntentEnviaParametros.kt
@@ -2,10 +2,19 @@ package com.example.moviles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class IntentEnviaParametros : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_envia_parametros)
+        // Propiedad Clase intent
+        // this.intent
+        val numeroEncontrado = intent.getIntExtra(
+            "numero",
+            0
+        )
+        Log.i("intents", "El numero encontrado es:${numeroEncontrado}")
+
     }
 }
